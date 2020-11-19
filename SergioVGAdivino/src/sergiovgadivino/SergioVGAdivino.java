08fc559c48f7bda5f20ff23d5544af287071afe9
Hector Cano <hectorcanomaestre@hotmail.com>
2020-11-19 23:06:27 +0100

    Añadido file.java a un proyecto

---
+++ SergioVGAdivino/src/sergiovgadivino/SergioVGAdivino.java
@@ -0,0 +1,28 @@
+
+package sergiovgadivino;
+
+import java.util.Scanner;
+
+public class SergioVGAdivino {
+
+    public static void main(String[] args) {
+      int n = 0,cont = 0;
+
+      int x = (int) (Math.random()*(41)+10);
+
+        //System.out.print(x);
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("\nIntroduce el valor de X: ");
+            n = sc.nextInt();
+            if (n > x) {
+                System.out.print("Te pasaste Amigo");
+            } else if (n < x){
+                System.out.print("Casi cerca ...");
+            }
+            cont++;
+        } while (n != x);
+        System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        sc.close();  
+    }
+}
